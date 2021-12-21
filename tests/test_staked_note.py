978d70e723b93cf8b2ee977af57cefb9244f7774
Jeff Wu <jeffywu@pm.me>
2021-12-21 13:30:47 -0800

    initial version of the test

--- tests/test_staked_note.py
+++ tests/test_staked_note.py
@@ -1,8 +1,9 @@
 import pytest
 import brownie
+import eth_abi
 from brownie import accounts, sNOTE
 from brownie.network.state import Chain
-from scripts.environment import TestAccounts, Environment, create_environment
+from scripts.environment import TestAccounts, Environment, create_environment, ETH_ADDRESS
 
 chain = Chain()
 @pytest.fixture(autouse=True)
@@ -71,8 +72,33 @@ def test_set_swap_fee_percentage():
     env.sNOTE.setSwapFeePercentage(0.03e18, {"from": env.deployer})
     assert env.balancerPool.getSwapFeePercentage() == 0.03e18
 
-# # User methods
-# def test_mint_from_bpt():
+# User methods
+@pytest.mark.only
+def test_mint_from_bpt():
+    env = create_environment()
+    testAccounts = TestAccounts()
+    env.note.transfer(testAccounts.ETHWhale, 100e8, {"from": env.deployer})
+    env.note.approve(env.balancerVault.address, 2**256-1, {"from": testAccounts.ETHWhale})
+
+    # [TOKEN_IN_FOR_EXACT_BPT_OUT, bptAmountOut, enterTokenIndex]
+    userData = eth_abi.encode_abi(
+        ['uint256', 'uint256', 'uint256'],
+        [2, 100, 1]
+    )
+
+    env.balancerVault.joinPool(
+        env.poolId,
+        testAccounts.ETHWhale,
+        testAccounts.ETHWhale,
+        (
+            [ETH_ADDRESS, env.note.address],
+            [0, 100],
+            userData,
+            False
+        ),
+        { "from": testAccounts.ETHWhale }
+    )
+
 # def test_mint_from_note():
 # def test_redeem():
 # def test_no_transfer_during_cooldown():
